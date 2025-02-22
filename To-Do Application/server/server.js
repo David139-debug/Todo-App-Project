51591b447f3ffeb80d14f4064775d7f4624e9f77
David139-debug <davidmalesevic55@gmail.com>
2025-02-22 19:50:53 +0100

    update

--- To-Do Application/server/server.js
+++ To-Do Application/server/server.js
@@ -6,7 +6,9 @@ const connectDB = require("./config/connectDB");
 const mongoose = require("mongoose");
 const PORT = 5000;
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://lambent-cuchufli-b15fe2.netlify.app/"],
+    origin: ["http://localhost:5173",
+             "https://lambent-cuchufli-b15fe2.netlify.app/",
+             "https://todo-app-project-eexq.onrender.com"],
     methods: "GET,POST,PUT,DELETE"
 }
 
