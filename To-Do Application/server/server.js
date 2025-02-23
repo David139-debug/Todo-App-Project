4722fb97bc24949c5f4ed9541c830e0f213d0474
David139-debug <davidmalesevic55@gmail.com>
2025-02-23 11:33:38 +0100

    Update server.js

--- To-Do Application/server/server.js
+++ To-Do Application/server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/connectDB");
 const mongoose = require("mongoose");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const corsOptions = {
     origin: ["http://localhost:5173",
              "https://lambent-cuchufli-b15fe2.netlify.app",
