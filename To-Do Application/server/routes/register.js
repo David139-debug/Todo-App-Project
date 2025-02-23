75560795b042806500ee81f99eddd88c970cb505
David139-debug <davidmalesevic55@gmail.com>
2025-02-23 11:37:32 +0100

    Update register.js

--- To-Do Application/server/routes/register.js
+++ To-Do Application/server/routes/register.js
@@ -4,4 +4,7 @@ const registerController = require("../controllers/registerController");
 
 router.post("/", registerController.handleRegister);
 
+router.get("/", (req, res) => {
+    res.send("Register route is working!");
+});
 module.exports = router
